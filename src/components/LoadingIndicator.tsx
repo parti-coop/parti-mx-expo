b53b7c5820f42fb0f9dbe05c89693a6254318bee
swain <iamswain@hotmail.com>
2019-12-09 01:25:03 -0800

    로딩 indicator추가

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { View } from "./View";
 import { Text } from "./Text";
+import { ActivityIndicator } from "react-native";
 export default () => {
   return (
-    <View style={{ alignItems: "center", justifyContent: "center" }}>
+    <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+      <ActivityIndicator size="large" color="#0000ff" />
       <Text>로딩중입니다.</Text>
     </View>
   );
