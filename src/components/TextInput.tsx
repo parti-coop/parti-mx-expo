bfade816f870d3a7728ff3b503d0cd76f6f3c487
swain <iamswain@hotmail.com>
2020-01-26 05:39:40 -0800

    개발 편의 위해 텍스트 박스에 칼러 넣기

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { TextInput as T, TextInputProps } from "react-native";
-
+import { getRandomColor } from "../Utils/RandomColorGenerator";
 export const TextInput = React.forwardRef<T, TextInputProps>(
   (props, ref: React.Ref<T>) => (
-    <T {...props} ref={ref}>
+    <T
+      {...props}
+      style={{ backgroundColor: getRandomColor(), ...(props.style as Object) }}
+      ref={ref}
+    >
       {props.children}
     </T>
   )
