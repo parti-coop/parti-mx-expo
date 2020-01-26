0ba5d75b1add17cce992ec6b515629151eaec9ee
swain <iamswain@hotmail.com>
2020-01-26 05:05:05 -0800

    회원가입 위해 firebase 추가

---
+++ src/screens/firebase.tsx
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJImw0nqvfEhKt9POJRuteOEVRXZyA27g",
+  authDomain: "parti-2020.firebaseapp.com",
+  databaseURL: "https://parti-2020.firebaseio.com",
+  projectId: "parti-2020",
+  storageBucket: "parti-2020.appspot.com",
+  messagingSenderId: "959324853924",
+  appId: "1:959324853924:web:0fdb1b2838fb9147b53831",
+  measurementId: "G-84TGWTQ100"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const auth = firebase.auth();
+
+export { auth };
