331a9314242a66bf48f3b747c92245b130d1a75a
swain <iamswain@hotmail.com>
2020-02-14 15:09:51 -0800

    change terms url

--- src/screens/TermsPrivacy.tsx
+++ src/screens/TermsPrivacy.tsx
@@ -3,5 +3,5 @@ import { WebView } from "react-native-webview";
 import { NavigationSwitchScreenProps } from "react-navigation";
 export default (props: NavigationSwitchScreenProps) => {
   const { navigate } = props.navigation;
-  return <WebView source={{ uri: "https://expo.io" }} />;
+  return <WebView source={{ uri: "https://github.com/parti-coop/expo-2020/wiki/빠띠-2020-개인정보처리방침" }} />;
 };
