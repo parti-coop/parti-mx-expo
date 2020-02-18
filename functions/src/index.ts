456104d51f15ea20a90233a126ca806288371577
Swain Hwang <swain@swainuicbookpro.lan>
2020-02-18 03:48:15 -0800

    cloud auth functions

---
+++ functions/src/index.ts
@@ -0,0 +1,45 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
+exports.hasuraWebhook = functions
+  .region("asia-northeast1")
+  .https.onRequest((request: any, response: any) => {
+    const authHeaders = request.get("Authorization");
+    // Send anonymous role if there are no auth headers
+    if (!authHeaders) {
+      response.json({ "x-hasura-role": "anonymous" });
+      return;
+    } else {
+      // Validate the received id_tokenolp;
+      const idToken = extractToken(authHeaders);
+      console.log(idToken);
+      admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then((decodedToken: any) => {
+          console.log("decodedToken", decodedToken);
+          const hasuraVariables = {
+            "X-Hasura-User-Id": decodedToken.uid,
+            "X-Hasura-Role": "user"
+          };
+          console.log(hasuraVariables); // For debug
+          // Send appropriate variables
+          response.json(hasuraVariables);
+          return;
+        })
+        .catch((e: Error) => {
+          // Throw authentication error
+          console.log(e);
+          response.json({ "x-hasura-role": "anonymous" });
+        });
+    }
+  });
+
+const extractToken = (bearerToken: any) => {
+  const regex = /^(Bearer) (.*)$/g;
+  const match = regex.exec(bearerToken);
+  if (match && match[2]) {
+    return match[2];
+  }
+  throw Error("Error 403 auth failed");
+};
