150a062c282169e758d5377434e9d739d13e3559
Swain Hwang <swain@swainuicbookpro.lan>
2020-03-16 22:21:44 -0700

    add type to .env

--- functions/src/env.ts
+++ functions/src/env.ts
@@ -1,5 +1,12 @@
 import * as functions from "firebase-functions";
 // export const HASURA_GRAPHQL_ENGINE_URL = process.env.hasura.url;
 // export const ADMIN_SECRET = process.env.hasura.secret;
-export const HASURA_GRAPHQL_ENGINE_URL = functions.config().hasura.url;
-export const ADMIN_SECRET = functions.config().hasura.secret;
+interface Config extends functions.config.Config {
+  hasura: {
+    url: string;
+    secret: string;
+  };
+}
+const config = functions.config() as Config;
+export const HASURA_GRAPHQL_ENGINE_URL = config.hasura.url;
+export const ADMIN_SECRET = config.hasura.secret;
