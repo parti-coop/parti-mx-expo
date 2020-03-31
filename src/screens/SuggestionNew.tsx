9451329238046972f5112dc8cbfe8b4c1f187034
Swain Hwang <swain@swainuicbookpro.lan>
2020-03-30 17:59:15 -0700

    작성 시 키보드 가리는 에러 해결

--- src/screens/SuggestionNew.tsx
+++ src/screens/SuggestionNew.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Image, StyleProp, TextStyle } from "react-native";
-import { StackHeaderProps } from "@react-navigation/stack";
-import RNPickerSelect from "react-native-picker-select";
+import { StyleProp, TextStyle, Keyboard } from "react-native";
 import { showMessage } from "react-native-flash-message";
 import { useMutation } from "@apollo/react-hooks";
 import { AutoGrowingTextInput } from "react-native-autogrow-textinput";
+import { useNavigation } from "@react-navigation/native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 import { Text } from "../components/Text";
 import { TextInput } from "../components/TextInput";
@@ -20,8 +20,8 @@ import { insertSuggestion } from "../graphql/mutation";
 
 const options = [
   { label: "30일 후 종료", value: 0 },
-  { label: "멤버 과반수 동의시 종료", value: 1 },
-  { label: "제안 정리시 종료", value: 2 }
+  // { label: "멤버 과반수 동의시 종료", value: 1 }
+  // { label: "제안 정리시 종료", value: 2 }
 ];
 const labelStyle: StyleProp<TextStyle> = {
   fontSize: 13,
@@ -37,7 +37,7 @@ const textStyle: StyleProp<TextStyle> = {
   flex: 1
 };
 
-export default (props: StackHeaderProps) => {
+export default () => {
   const [insert, { loading }] = useMutation(insertSuggestion);
   const [{ board_id, user_id }, dispatch] = useStore();
   const [sTitle, setSTitle] = React.useState("");
@@ -45,7 +45,9 @@ export default (props: StackHeaderProps) => {
   const [sBody, setSBody] = React.useState("");
   const [closingMethod, setClosingMethod] = React.useState(0);
   const contextRef = React.useRef(null);
-  function insertPressHandler() {
+  const scrollRef = React.useRef(null);
+  const { navigate } = useNavigation();
+  async function insertPressHandler() {
     if (sTitle.trim() == "" || sTitle.trim().length > 20) {
       return showMessage({
         message: "제안명을 20자 이내로 입력해주세요.",
@@ -58,7 +60,7 @@ export default (props: StackHeaderProps) => {
         type: "warning"
       });
     }
-    insert({
+    await insert({
       variables: {
         sTitle,
         sContext,
@@ -67,9 +69,9 @@ export default (props: StackHeaderProps) => {
         user_id,
         closingMethod
       }
-    })
-      .then(res => alert(JSON.stringify(res.data)))
-      .then(e => props.navigation.goBack());
+    });
+    // Keyboard.dismiss();
+    navigate("SuggestionList");
   }
   React.useEffect(() => {
     dispatch({ type: "SET_LOADING", loading });
@@ -78,141 +80,148 @@ export default (props: StackHeaderProps) => {
   return (
     <>
       <HeaderNew insert={insertPressHandler} />
-      <HeaderSuggestion />
-      <View
-        style={{ paddingHorizontal: 28, paddingBottom: 30, paddingTop: 20 }}
+      <KeyboardAwareScrollView
+        keyboardShouldPersistTaps={"handled"}
+        ref={scrollRef}
+        extraScrollHeight={200}
+        enableAutomaticScroll={true}
       >
-        <Text
-          style={{
-            fontSize: 22,
-            textAlign: "left",
-            color: "#333333"
-          }}
+        <HeaderSuggestion />
+        <View
+          style={{ paddingHorizontal: 28, paddingBottom: 30, paddingTop: 20 }}
         >
-          글 쓰기
-        </Text>
-      </View>
-      <View
-        style={{
-          alignItems: "stretch",
-          borderRadius: 25,
-          backgroundColor: "#ffffff",
-          shadowColor: "rgba(0, 0, 0, 0.15)",
-          shadowOffset: {
-            width: 0,
-            height: 1
-          },
-          shadowRadius: 1,
-          shadowOpacity: 1
-        }}
-      >
-        <ViewRow style={{ paddingHorizontal: 30 }}>
-          <Text style={[labelStyle, { paddingVertical: 24 }]}>제안명</Text>
-          <TextInput
-            value={sTitle}
-            autoFocus
-            onChangeText={setSTitle}
-            placeholderTextColor="#999999"
-            style={textStyle}
-            onSubmitEditing={() => contextRef.current.focus()}
-          />
-        </ViewRow>
-        <LineSeperator />
-        <ViewRow
-          style={{
-            paddingHorizontal: 30
-          }}
-        >
-          <Text style={labelStyle}>종료 방법</Text>
-          <TouchableClosingMethod
-            value={closingMethod}
-            onChange={setClosingMethod}
-            items={options}
-          />
-        </ViewRow>
-      </View>
-      <View
-        style={{
-          marginTop: 10,
-          alignItems: "stretch",
-          borderRadius: 25,
-          backgroundColor: "#ffffff",
-          shadowColor: "rgba(0, 0, 0, 0.15)",
-          shadowOffset: {
-            width: 0,
-            height: 1
-          },
-          shadowRadius: 1,
-          shadowOpacity: 1,
-          flex: 1
-        }}
-      >
-        <View style={{ padding: 30, paddingBottom: 20, flex: 1 }}>
-          <Text style={[labelStyle, { paddingBottom: 19 }]}>제안 배경</Text>
-          <AutoGrowingTextInput
-            value={sContext}
-            multiline
-            textAlignVertical="top"
-            placeholder="제안 배경을 입력해 주세요"
-            placeholderTextColor="#999999"
-            onChangeText={setSContext}
-            style={[textStyle]}
-            ref={contextRef}
-          />
+          <Text
+            style={{
+              fontSize: 22,
+              textAlign: "left",
+              color: "#333333"
+            }}
+          >
+            글 쓰기
+          </Text>
         </View>
-        <LineSeperator />
         <View
           style={{
-            padding: 30,
-            paddingBottom: 20,
+            alignItems: "stretch",
+            borderRadius: 25,
+            backgroundColor: "#ffffff",
+            shadowColor: "rgba(0, 0, 0, 0.15)",
+            shadowOffset: {
+              width: 0,
+              height: 1
+            },
+            shadowRadius: 1,
+            shadowOpacity: 1
+          }}
+        >
+          <ViewRow style={{ paddingHorizontal: 30 }}>
+            <Text style={[labelStyle, { paddingVertical: 24 }]}>제안명</Text>
+            <TextInput
+              value={sTitle}
+              autoFocus
+              onChangeText={setSTitle}
+              placeholderTextColor="#999999"
+              style={textStyle}
+              onSubmitEditing={() => contextRef.current.focus()}
+            />
+          </ViewRow>
+          <LineSeperator />
+          <ViewRow
+            style={{
+              paddingHorizontal: 30
+            }}
+          >
+            <Text style={labelStyle}>종료 방법</Text>
+            <TouchableClosingMethod
+              value={closingMethod}
+              onChange={setClosingMethod}
+              items={options}
+            />
+          </ViewRow>
+        </View>
+        <View
+          style={{
+            marginTop: 10,
+            alignItems: "stretch",
+            borderRadius: 25,
+            backgroundColor: "#ffffff",
+            shadowColor: "rgba(0, 0, 0, 0.15)",
+            shadowOffset: {
+              width: 0,
+              height: 1
+            },
+            shadowRadius: 1,
+            shadowOpacity: 1,
             flex: 1
           }}
         >
-          <Text style={[labelStyle, { paddingBottom: 19 }]}>제안 내용</Text>
-          <AutoGrowingTextInput
-            value={sBody}
-            multiline
-            textAlignVertical="top"
-            placeholder="제안 내용을 입력해 주세요"
-            placeholderTextColor="#999999"
-            onChangeText={setSBody}
-            style={[textStyle]}
-          />
-        </View>
-        <LineSeperator />
-        <ViewRow style={{ padding: 22 }}>
-          <TO1>
-            <Text
-              style={{
-                fontSize: 16,
-                textAlign: "center",
-                color: "#30ad9f"
-              }}
-            >
-              사진 첨부
-            </Text>
-          </TO1>
+          <View style={{ padding: 30, paddingBottom: 20, flex: 1 }}>
+            <Text style={[labelStyle, { paddingBottom: 19 }]}>제안 배경</Text>
+            <AutoGrowingTextInput
+              value={sContext}
+              multiline
+              textAlignVertical="top"
+              placeholder="제안 배경을 입력해 주세요"
+              placeholderTextColor="#999999"
+              onChangeText={setSContext}
+              style={[textStyle]}
+              ref={contextRef}
+            />
+          </View>
+          <LineSeperator />
           <View
             style={{
-              width: 1,
-              height: 11,
-              backgroundColor: "#e4e4e4"
+              padding: 30,
+              paddingBottom: 20,
+              flex: 1
             }}
-          />
-          <TO1>
-            <Text
+          >
+            <Text style={[labelStyle, { paddingBottom: 19 }]}>제안 내용</Text>
+            <AutoGrowingTextInput
+              value={sBody}
+              multiline
+              textAlignVertical="top"
+              placeholder="제안 내용을 입력해 주세요"
+              placeholderTextColor="#999999"
+              onChangeText={setSBody}
+              style={[textStyle]}
+            />
+          </View>
+          <LineSeperator />
+          <ViewRow style={{ padding: 22 }}>
+            <TO1>
+              <Text
+                style={{
+                  fontSize: 16,
+                  textAlign: "center",
+                  color: "#30ad9f"
+                }}
+              >
+                사진 첨부
+              </Text>
+            </TO1>
+            <View
               style={{
-                fontSize: 16,
-                textAlign: "center",
-                color: "#30ad9f",
-                flex: 1
+                width: 1,
+                height: 11,
+                backgroundColor: "#e4e4e4"
               }}
-            >
-              파일 첨부
-            </Text>
-          </TO1>
-        </ViewRow>
-      </View>
+            />
+            <TO1>
+              <Text
+                style={{
+                  fontSize: 16,
+                  textAlign: "center",
+                  color: "#30ad9f",
+                  flex: 1
+                }}
+              >
+                파일 첨부
+              </Text>
+            </TO1>
+          </ViewRow>
+        </View>
+      </KeyboardAwareScrollView>
     </>
   );
 };
