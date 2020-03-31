ec64bca55cc91c50bdaa58bca9481081421d4391
Swain Hwang <swain@swainuicbookpro.lan>
2020-03-30 17:59:33 -0700

    improve delete

--- src/components/HooksDeleteSuggestion.tsx
+++ src/components/HooksDeleteSuggestion.tsx
@@ -2,19 +2,28 @@ import React from "react";
 import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useMutation } from "@apollo/react-hooks";
+import { showMessage } from "react-native-flash-message";
 
 import { useStore } from "../Store";
 import { deleteSuggestion } from "../graphql/mutation";
 
 export default (id: number) => {
   const [, dispatch] = useStore();
-  const { goBack } = useNavigation();
+  const { navigate } = useNavigation();
   const [del, { loading }] = useMutation(deleteSuggestion, {
     variables: { id }
   });
   React.useEffect(() => {
     dispatch({ type: "SET_LOADING", loading });
   }, [loading]);
+  async function deleteHandler() {
+    await del();
+    showMessage({
+      type: "success",
+      message: "삭제 되었습니다"
+    });
+    navigate("SuggestionList");
+  }
 
   function showAlert() {
     return Alert.alert("제안 삭제", "삭제하겠습니까? 복구할 수 없습니다.", [
@@ -24,11 +33,7 @@ export default (id: number) => {
       },
       {
         text: "삭제",
-        onPress: () =>
-          del()
-            .then(() => alert("삭제되었습니다."))
-            .catch(console.error)
-            .finally(goBack)
+        onPress: deleteHandler
       }
     ]);
   }
