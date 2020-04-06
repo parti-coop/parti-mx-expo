cd46be7915a8f0465705020aea6e6826e3f434d2
Swain Hwang <swain@swainuicbookpro.lan>
2020-04-05 18:28:01 -0700

    password change publishing

--- src/screens/PasswordChange.tsx
+++ src/screens/PasswordChange.tsx
@@ -1,66 +1,91 @@
 import React from "react";
-import { Alert } from "react-native";
-import { AntDesign, Ionicons } from "@expo/vector-icons";
-import { useStore } from "../Store";
-import { Text, Text2 } from "../components/Text";
-import { View, ViewRow, ViewColumnCenter } from "../components/View";
-import { TouchableOpacity, TO1 } from "../components/TouchableOpacity";
+import { TouchableWithoutFeedback, Keyboard, ViewProps } from "react-native";
+import { showMessage } from "react-native-flash-message";
+
+import { Mint13, Title30 } from "../components/Text";
+import { View, ViewRow } from "../components/View";
+import HeaderConfirm from "../components/HeaderConfirm";
 import { PasswordInput } from "../components/TextInput";
+import { KeyboardAwareScrollView } from "../components/KeyboardAwareScrollView";
+
+import { useStore } from "../Store";
 import { auth, Firebase } from "../firebase";
-export default props => {
+
+const boxStyle = {
+  borderRadius: 25,
+  backgroundColor: "#ffffff",
+  shadowColor: "rgba(0, 0, 0, 0.15)",
+  shadowOffset: {
+    width: 0,
+    height: 1,
+  },
+  shadowRadius: 1,
+  shadowOpacity: 1,
+  marginHorizontal: 30,
+  marginVertical: 21,
+  justifyContent: "center",
+  padding: 30,
+} as ViewProps;
+export default (props) => {
   const { navigate, goBack } = props.navigation;
   const [store, dispatch] = useStore();
   const [oldP, setOldP] = React.useState("");
   const [newP, setNewP] = React.useState("");
-
-  function reauth() {
+  function resetInput() {
+    setOldP("");
+    setNewP("");
+  }
+  async function reauth() {
+    if (!(oldP && newP)) {
+      return showMessage({ type: "info", message: "모두 입력하십시오" });
+    }
     const credential = Firebase.auth.EmailAuthProvider.credential(
       auth.currentUser.email,
       oldP
     );
-    auth.currentUser
-      .reauthenticateWithCredential(credential)
-      .then(() => auth.currentUser.updatePassword(newP))
-      .then(() =>
-        Alert.alert("갱신 성공", "비밀번호 갱신에 성공했습니다.", [
-          {
-            text: "확인",
-            onPress: () => goBack()
-          }
-        ])
-      )
-      .catch(err =>
-        Alert.alert(
-          "갱신 실패",
-          "오류가 발생했습니다. 잠시 후 다시 시도하세요." + err,
-          [
-            {
-              text: "확인",
-              onPress: () => goBack()
-            }
-          ]
-        )
-      );
+    dispatch({ type: "SET_LOADING", loading: true });
+    resetInput();
+    try {
+      await auth.currentUser.reauthenticateWithCredential(credential);
+      await auth.currentUser.updatePassword(newP);
+      showMessage({
+        type: "success",
+        message: "비밀번호 갱신에 성공했습니다.",
+      });
+    } catch (error) {
+      showMessage({ type: "danger", message: error.message });
+    }
+    dispatch({ type: "SET_LOADING", loading: false });
   }
   return (
     <>
-      <ViewRow>
-        <TouchableOpacity
-          style={{ width: 50, padding: 10 }}
-          onPress={() => props.navigation.goBack()}
-        >
-          <Ionicons name="ios-arrow-back" size={60} />
-        </TouchableOpacity>
-        <Text2>비밀번호 변경</Text2>
-      </ViewRow>
+      <HeaderConfirm onPress={reauth} />
+      <KeyboardAwareScrollView>
+        <View style={{ padding: 30, paddingTop: 6 }}>
+          <Title30>비밀번호 변경</Title30>
+        </View>
 
-      <View style={{ flex: 1, alignItems:"stretch" }}>
-        <PasswordInput value={oldP} onChangeText={setOldP} />
-        <PasswordInput value={newP} onChangeText={setNewP} />
-      </View>
-      <TO1 onPress={reauth}>
-        <Text>보내기</Text>
-      </TO1>
+        <View style={boxStyle}>
+          <ViewRow>
+            <Mint13>현재 비밀번호</Mint13>
+            <PasswordInput
+              value={oldP}
+              onChangeText={setOldP}
+              placeholder="현재 비밀번호를입력해 주세요"
+              style={{ fontSize: 16 }}
+            />
+          </ViewRow>
+          <ViewRow>
+            <Mint13>신규 비밀번호</Mint13>
+            <PasswordInput
+              value={newP}
+              onChangeText={setNewP}
+              placeholder="신규 비밀번호를 입력해주세요"
+              style={{ fontSize: 16 }}
+            />
+          </ViewRow>
+        </View>
+      </KeyboardAwareScrollView>
     </>
   );
 };
