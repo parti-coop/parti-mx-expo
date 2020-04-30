cd8ba8cbfdf48bc6144c177a25ace735229b8404
Swain Hwang <swain@swainuicbookpro.lan>
2020-04-29 21:41:15 -0700

    terms publishing

--- src/screens/TermsPrivacy.tsx
+++ src/screens/TermsPrivacy.tsx
@@ -1,23 +1,43 @@
 import React from "react";
-import { WebView } from "react-native-webview";
+import { ScrollView, ViewStyle } from "react-native";
 
-import { ViewRow, V0, View } from "../components/View";
-import { Text, Title30 } from "../components/Text";
-
+import { ViewRow } from "../components/View";
+import { Title30 } from "../components/Text";
+import Markdown from "react-native-markdown-display";
 import HeaderBack from "../components/HeaderBack";
+const box = {
+  borderRadius: 25,
+  backgroundColor: "#ffffff",
+  shadowColor: "rgba(0, 0, 0, 0.15)",
+  elevation: 1,
+  shadowOffset: {
+    width: 0,
+    height: 1,
+  },
+  shadowRadius: 1,
+  shadowOpacity: 1,
+  padding: 30,
+} as ViewStyle;
 export default () => {
+  const [md, setMd] = React.useState("");
+  React.useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/wiki/parti-coop/parti-mx/개인정보처리방침.md"
+    )
+      .then((res) => res.text())
+      .then(setMd);
+  }, []);
   return (
     <>
       <HeaderBack />
       <ViewRow style={{ padding: 30, paddingTop: 6 }}>
         <Title30>개인정보처리방침</Title30>
       </ViewRow>
-      <WebView
-        source={{
-          uri:
-            "https://github.com/parti-coop/expo-2020/wiki/빠띠-2020-개인정보처리방침"
-        }}
-      />
+      <ScrollView style={box}>
+        <Markdown style={{ body: { fontSize: 16, color: "#555555" } }}>
+          {md}
+        </Markdown>
+      </ScrollView>
     </>
   );
 };
