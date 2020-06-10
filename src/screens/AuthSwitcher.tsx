0c3f0395fd7b11db34ec44cc44029c6314d1f8ca
Swain Hwang <swain@swainuicbookpro.lan>
2020-06-09 21:23:37 -0700

    add linking to container

--- src/screens/AuthSwitcher.tsx
+++ src/screens/AuthSwitcher.tsx
@@ -4,14 +4,23 @@ import AppContainer from "./AppContainer";
 import AuthContainer from "./AuthContainer";
 import { auth } from "../firebase";
 import { useStore } from "../Store";
+const linking = {
+  prefixes: ["https://parti.mx", "parti-mx://"],
+  config: {
+    Profile: "profile",
+    Login: "login",
+    SignupEmail: "SignupEmail",
+  },
+};
 export default function AuthSwithcer() {
   const [user, setUser] = React.useState(undefined);
   const [{ user_id }] = useStore();
   React.useEffect(() => {
     return auth.onAuthStateChanged(setUser);
   }, []);
+
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       {user && user_id !== null ? <AppContainer /> : <AuthContainer />}
     </NavigationContainer>
   );
